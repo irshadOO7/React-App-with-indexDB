9fe47d4cc9c95ff266f9dbe599d7438cbd4bfa66
irshad <coolirshadalam@gmail.com>
2023-05-06 20:07:57 +0530

    Final submit

---
+++ src/data.js
@@ -0,0 +1,16 @@
+export const USER_DATA = [
+  {
+    id: 1,
+    firstName: "Irshad",
+    lastName: "Alam",
+    email: "irshad@test.com",
+    age: 25
+  },
+  {
+    id: 2,
+    firstName: "Saif",
+    lastName: "Ali",
+    email: "saif@test.com",
+    age: 21
+  },
+];
